76889ecd5726dd7637219b6056e86c35a0e981b6
Peter O'Connor <poc275@gmail.com>
2021-06-20 12:52:15 +0100

    Adds reflector relative offset.

--- src/enigma/Reflector.js
+++ src/enigma/Reflector.js
@@ -1,3 +1,5 @@
+import Utility from "./Utility";
+
 class Reflector {
 
     constructor() {
@@ -5,10 +7,21 @@ class Reflector {
             ["A", "Y"], ["B", "R"], ["C", "U"], ["D", "H"], ["E", "Q"], ["F", "S"], ["G", "L"], ["H", "D"], ["I", "P"], ["J", "X"], ["K", "N"], ["L", "G"], ["M", "O"], 
             ["N", "K"], ["O", "M"], ["P", "I"], ["Q", "E"], ["R", "B"], ["S", "F"], ["T", "Z"], ["U", "C"], ["V", "W"], ["W", "V"], ["X", "J"], ["Y", "A"], ["Z", "T"]
         ]);
+        this._relativeOffset = 0;
+    }
+
+    get relativeOffset() {
+        return this._relativeOffset;
+    }
+
+    set relativeOffset(offset) {
+        this._relativeOffset = offset;
     }
 
     reflect(input) {
-        return this._reflectorB.get(input);
+        // apply relative offset before reflecting
+        const shiftedInput = Utility.getShiftedLetter(input, this.relativeOffset);
+        return this._reflectorB.get(shiftedInput);
     }
 }
 
--- src/enigma/__tests__/Reflector.test.js
+++ src/enigma/__tests__/Reflector.test.js
@@ -5,6 +5,19 @@ test("instance is correct", () => {
     expect(reflector).toBeInstanceOf(Reflector);
 });
 
+test("relative offset is initialised to zero", () => {
+    const reflector = new Reflector();
+    expect(reflector.relativeOffset).toEqual(0);
+});
+
+test("relative offset setter sets the correct value", () => {
+    const reflector = new Reflector();
+    expect(reflector.relativeOffset).toEqual(0);
+
+    reflector.relativeOffset = 9;
+    expect(reflector.relativeOffset).toEqual(9);
+});
+
 test("reflect returns a single letter", () => {
     const reflector = new Reflector();
     const output = reflector.reflect("A");
@@ -21,5 +34,20 @@ ${"C"}   | ${"U"}
 `("reflect returns $expected when input letter is $input", ({input, expected}) => {
     const reflector = new Reflector();
     const output = reflector.reflect(input);
+    expect(output).toEqual(expected);
+});
+
+test.each`
+input    | relativeOffset | expected
+${"A"}   | ${1}           | ${"R"}
+${"F"}   | ${2}           | ${"D"}
+${"P"}   | ${12}          | ${"R"}
+${"A"}   | ${25}          | ${"T"}
+${"B"}   | ${25}          | ${"Y"}
+`("reflect returns $expected when input letter is $input and relative offset is $relativeOffset", ({input, relativeOffset, expected}) => {
+    const reflector = new Reflector();
+    reflector.relativeOffset = relativeOffset;
+    const output = reflector.reflect(input);
+
     expect(output).toEqual(expected);
 });
