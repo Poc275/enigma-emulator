3bd3b41800c5a25ee11461a7b056c8c628a9a1d9
Peter O'Connor <poc275@gmail.com>
2021-06-25 17:30:21 +0100

    Adds different reflector settings.

--- src/enigma/Reflector.js
+++ src/enigma/Reflector.js
@@ -2,11 +2,30 @@ import Utility from "./Utility";
 
 class Reflector {
 
-    constructor() {
-        this._reflectorB = new Map([
+    constructor(reflector) {
+        const reflectorASettings = new Map([
+            ["A", "E"], ["B", "J"], ["C", "M"], ["D", "Z"], ["E", "A"], ["F", "L"], ["G", "Y"], ["H", "X"], ["I", "V"], ["J", "B"], ["K", "W"], ["L", "F"], ["M", "C"], 
+            ["N", "R"], ["O", "Q"], ["P", "U"], ["Q", "O"], ["R", "N"], ["S", "T"], ["T", "S"], ["U", "P"], ["V", "I"], ["W", "K"], ["X", "H"], ["Y", "G"], ["Z", "D"]
+        ]);
+
+        const reflectorBSettings = new Map([
             ["A", "Y"], ["B", "R"], ["C", "U"], ["D", "H"], ["E", "Q"], ["F", "S"], ["G", "L"], ["H", "D"], ["I", "P"], ["J", "X"], ["K", "N"], ["L", "G"], ["M", "O"], 
             ["N", "K"], ["O", "M"], ["P", "I"], ["Q", "E"], ["R", "B"], ["S", "F"], ["T", "Z"], ["U", "C"], ["V", "W"], ["W", "V"], ["X", "J"], ["Y", "A"], ["Z", "T"]
         ]);
+        
+        switch(reflector) {
+            case "A":
+                this._reflectorSettings = reflectorASettings;
+                break;
+
+            case "B":
+                this._reflectorSettings = reflectorBSettings;
+                break;
+
+            default:
+                throw new Error(`Unknown reflector ${reflector}. Valid reflectors are "A" and "B".`);
+        }
+
         this._relativeOffset = 0;
     }
 
@@ -18,10 +37,14 @@ class Reflector {
         this._relativeOffset = offset;
     }
 
+    get reflectorSettings() {
+        return this._reflectorSettings;
+    }
+
     reflect(input) {
         // apply relative offset before reflecting
         const shiftedInput = Utility.getShiftedLetter(input, this.relativeOffset);
-        return this._reflectorB.get(shiftedInput);
+        return this.reflectorSettings.get(shiftedInput);
     }
 }
 
--- src/enigma/__tests__/Reflector.test.js
+++ src/enigma/__tests__/Reflector.test.js
@@ -1,17 +1,37 @@
 import Reflector from "../Reflector";
 
 test("instance is correct", () => {
-    const reflector = new Reflector();
+    const reflector = new Reflector("A");
     expect(reflector).toBeInstanceOf(Reflector);
 });
 
+test("reflector A settings are applied correctly", () => {
+    const reflector = new Reflector("A");
+    expect(reflector.reflectorSettings).toEqual(new Map([
+        ["A", "E"], ["B", "J"], ["C", "M"], ["D", "Z"], ["E", "A"], ["F", "L"], ["G", "Y"], ["H", "X"], ["I", "V"], ["J", "B"], ["K", "W"], ["L", "F"], ["M", "C"], 
+        ["N", "R"], ["O", "Q"], ["P", "U"], ["Q", "O"], ["R", "N"], ["S", "T"], ["T", "S"], ["U", "P"], ["V", "I"], ["W", "K"], ["X", "H"], ["Y", "G"], ["Z", "D"]
+    ]));
+});
+
+test("reflector B settings are applied correctly", () => {
+    const reflector = new Reflector("B");
+    expect(reflector.reflectorSettings).toEqual(new Map([
+        ["A", "Y"], ["B", "R"], ["C", "U"], ["D", "H"], ["E", "Q"], ["F", "S"], ["G", "L"], ["H", "D"], ["I", "P"], ["J", "X"], ["K", "N"], ["L", "G"], ["M", "O"], 
+        ["N", "K"], ["O", "M"], ["P", "I"], ["Q", "E"], ["R", "B"], ["S", "F"], ["T", "Z"], ["U", "C"], ["V", "W"], ["W", "V"], ["X", "J"], ["Y", "A"], ["Z", "T"]
+    ]));
+});
+
+test("unknown reflector in the constructor throws an error", () => {
+    expect(() => new Reflector("NA")).toThrow();
+});
+
 test("relative offset is initialised to zero", () => {
-    const reflector = new Reflector();
+    const reflector = new Reflector("A");
     expect(reflector.relativeOffset).toEqual(0);
 });
 
 test("relative offset setter sets the correct value", () => {
-    const reflector = new Reflector();
+    const reflector = new Reflector("A");
     expect(reflector.relativeOffset).toEqual(0);
 
     reflector.relativeOffset = 9;
@@ -19,9 +39,22 @@ test("relative offset setter sets the correct value", () => {
 });
 
 test("reflect returns a single letter", () => {
-    const reflector = new Reflector();
+    const reflector = new Reflector("A");
     const output = reflector.reflect("A");
     expect(output.length).toEqual(1);
+});
+
+test.each`
+input    | expected
+${"A"}   | ${"E"}
+${"Y"}   | ${"G"}
+${"X"}   | ${"H"}
+${"J"}   | ${"B"}
+${"E"}   | ${"A"}
+`("reflect returns $expected when input letter is $input for reflector A", ({input, expected}) => {
+    const reflector = new Reflector("A");
+    const output = reflector.reflect(input);
+    expect(output).toEqual(expected);
 });
 
 test.each`
@@ -31,9 +64,24 @@ ${"Y"}   | ${"A"}
 ${"X"}   | ${"J"}
 ${"J"}   | ${"X"}
 ${"C"}   | ${"U"}
-`("reflect returns $expected when input letter is $input", ({input, expected}) => {
-    const reflector = new Reflector();
+`("reflect returns $expected when input letter is $input for reflector B", ({input, expected}) => {
+    const reflector = new Reflector("B");
     const output = reflector.reflect(input);
+    expect(output).toEqual(expected);
+});
+
+test.each`
+input    | relativeOffset | expected
+${"A"}   | ${1}           | ${"J"}
+${"F"}   | ${2}           | ${"X"}
+${"P"}   | ${12}          | ${"J"}
+${"A"}   | ${25}          | ${"D"}
+${"B"}   | ${25}          | ${"E"}
+`("reflect returns $expected when input letter is $input and relative offset is $relativeOffset for reflector A", ({input, relativeOffset, expected}) => {
+    const reflector = new Reflector("A");
+    reflector.relativeOffset = relativeOffset;
+    const output = reflector.reflect(input);
+
     expect(output).toEqual(expected);
 });
 
@@ -44,8 +92,8 @@ ${"F"}   | ${2}           | ${"D"}
 ${"P"}   | ${12}          | ${"R"}
 ${"A"}   | ${25}          | ${"T"}
 ${"B"}   | ${25}          | ${"Y"}
-`("reflect returns $expected when input letter is $input and relative offset is $relativeOffset", ({input, relativeOffset, expected}) => {
-    const reflector = new Reflector();
+`("reflect returns $expected when input letter is $input and relative offset is $relativeOffset for reflector B", ({input, relativeOffset, expected}) => {
+    const reflector = new Reflector("B");
     reflector.relativeOffset = relativeOffset;
     const output = reflector.reflect(input);
 
