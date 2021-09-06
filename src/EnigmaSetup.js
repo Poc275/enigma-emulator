ecacd98e60aa8abd6451db52defdbc6df2ea2a5a
Peter O'Connor <poc275@gmail.com>
2021-09-06 11:06:16 +0100

    Adds plugboard settings.

--- src/EnigmaSetup.js
+++ src/EnigmaSetup.js
@@ -64,16 +64,16 @@ function EnigmaSetup(props) {
         // console.log(event.target.value);
         switch(rotor) {
             case "lhs":
-            setLhsRotorPosition(event.target.value);
-            break;
+              setLhsRotorPosition(event.target.value);
+              break;
 
             case "mid":
-            setMidRotorPosition(event.target.value);
-            break;
+              setMidRotorPosition(event.target.value);
+              break;
 
             case "rhs":
-            setRhsRotorPosition(event.target.value);
-            break;
+              setRhsRotorPosition(event.target.value);
+              break;
         }
     };
     
@@ -166,7 +166,17 @@ function EnigmaSetup(props) {
         const midRotor = new Rotor(getRotorMapping(midSelectedRotor), midRotorPosition, parseInt(midRotorRingSetting, 10), getRotorStepPoints(midSelectedRotor));
         const rhsRotor = new Rotor(getRotorMapping(rhsSelectedRotor), rhsRotorPosition, parseInt(rhsRotorRingSetting, 10), getRotorStepPoints(rhsSelectedRotor));
 
-        const enigma = new Enigma(new Plugboard(), new EntryRotor(), rhsRotor, midRotor, lhsRotor, reflector);
+        // get plugboard settings
+        const plugboard = new Plugboard();
+        const plugboardInputs = document.querySelectorAll(".plugboard-input");
+        const plugboardSettingRegex = /[A-Z]{2}/g;
+        for(const plugboardInput of plugboardInputs) {
+          if(plugboardInput.value.match(plugboardSettingRegex)) {
+            plugboard.addSetting(plugboardInput.value[0], plugboardInput.value[1]);
+          }
+        }
+
+        const enigma = new Enigma(plugboard, new EntryRotor(), rhsRotor, midRotor, lhsRotor, reflector);
         setEngima(enigma);
 
         // call the setup callback so we can begin a new message
@@ -181,45 +191,59 @@ function EnigmaSetup(props) {
                 <br />
 
                 <p>
-                    <label htmlFor="reflector-select">1. Choose a reflector: </label>
-                    <select name="reflector" id="reflector-select" defaultValue={"B"} disabled={disableSettings}>
-                        <option value="A">Reflector A</option>
-                        <option value="B">Reflector B</option>
-                    </select>
+                  <label htmlFor="reflector-select">1. Choose a reflector: </label>
+                  <select name="reflector" id="reflector-select" defaultValue={"B"} disabled={disableSettings}>
+                    <option value="A">Reflector A</option>
+                    <option value="B">Reflector B</option>
+                  </select>
                 </p>
 
                 <p>
-                    <label htmlFor="lhs-rotor-select">2. Choose a left hand rotor/start position/ring setting: </label>
-                    <RotorSelect defaultValue={lhsSelectedRotor} onChange={(event) => rotorSelectionChange(event, "lhs")} disabled={disableSettings} />
-                    <PositionSelect defaultValue={lhsRotorPosition} onChange={(event) => rotorPositionChange(event, "lhs")} disabled={disableSettings} />
-                    <RingSettingSelect defaultValue={lhsRotorRingSetting} onChange={(event) => rotorRingSettingChange(event, "lhs")} disabled={disableSettings} />
+                  <label htmlFor="lhs-rotor-select">2. Choose a left hand rotor/start position/ring setting: </label>
+                  <RotorSelect defaultValue={lhsSelectedRotor} onChange={(event) => rotorSelectionChange(event, "lhs")} disabled={disableSettings} />
+                  <PositionSelect defaultValue={lhsRotorPosition} onChange={(event) => rotorPositionChange(event, "lhs")} disabled={disableSettings} />
+                  <RingSettingSelect defaultValue={lhsRotorRingSetting} onChange={(event) => rotorRingSettingChange(event, "lhs")} disabled={disableSettings} />
                 </p>
 
                 <p>
-                    <label htmlFor="middle-rotor-select">3. Choose a middle rotor/start position/ring setting: </label>
-                    <RotorSelect defaultValue={midSelectedRotor} onChange={(event) => rotorSelectionChange(event, "mid")} disabled={disableSettings} />
-                    <PositionSelect defaultValue={midRotorPosition} onChange={(event) => rotorPositionChange(event, "mid")} disabled={disableSettings} />
-                    <RingSettingSelect defaultValue={midRotorRingSetting} onChange={(event) => rotorRingSettingChange(event, "mid")} disabled={disableSettings} />
+                  <label htmlFor="middle-rotor-select">3. Choose a middle rotor/start position/ring setting: </label>
+                  <RotorSelect defaultValue={midSelectedRotor} onChange={(event) => rotorSelectionChange(event, "mid")} disabled={disableSettings} />
+                  <PositionSelect defaultValue={midRotorPosition} onChange={(event) => rotorPositionChange(event, "mid")} disabled={disableSettings} />
+                  <RingSettingSelect defaultValue={midRotorRingSetting} onChange={(event) => rotorRingSettingChange(event, "mid")} disabled={disableSettings} />
                 </p>
 
                 <p>
-                    <label htmlFor="rhs-rotor-select">4. Choose a right hand rotor/start position/ring setting: </label>
-                    <RotorSelect defaultValue={rhsSelectedRotor} onChange={(event) => rotorSelectionChange(event, "rhs")} disabled={disableSettings} />
-                    <PositionSelect defaultValue={rhsRotorPosition} onChange={(event) => rotorPositionChange(event, "rhs")} disabled={disableSettings} />
-                    <RingSettingSelect defaultValue={rhsRotorRingSetting} onChange={(event) => rotorRingSettingChange(event, "rhs")} disabled={disableSettings} />
+                  <label htmlFor="rhs-rotor-select">4. Choose a right hand rotor/start position/ring setting: </label>
+                  <RotorSelect defaultValue={rhsSelectedRotor} onChange={(event) => rotorSelectionChange(event, "rhs")} disabled={disableSettings} />
+                  <PositionSelect defaultValue={rhsRotorPosition} onChange={(event) => rotorPositionChange(event, "rhs")} disabled={disableSettings} />
+                  <RingSettingSelect defaultValue={rhsRotorRingSetting} onChange={(event) => rotorRingSettingChange(event, "rhs")} disabled={disableSettings} />
+                </p>
+
+                <p>
+                  <label htmlFor="plugboard-1">5. (optional) Enter plugboard settings (Up to 10 letter pairs which are swapped during message enciphering):</label>
+                  <input type="text" id="plugboard-1" name="plugboard-1" minLength="2" maxLength="2" size="2" disabled={disableSettings} className="plugboard-input"></input>
+                  <input type="text" id="plugboard-2" name="plugboard-2" minLength="2" maxLength="2" size="2" disabled={disableSettings} className="plugboard-input"></input>
+                  <input type="text" id="plugboard-3" name="plugboard-3" minLength="2" maxLength="2" size="2" disabled={disableSettings} className="plugboard-input"></input>
+                  <input type="text" id="plugboard-4" name="plugboard-4" minLength="2" maxLength="2" size="2" disabled={disableSettings} className="plugboard-input"></input>
+                  <input type="text" id="plugboard-5" name="plugboard-5" minLength="2" maxLength="2" size="2" disabled={disableSettings} className="plugboard-input"></input>
+                  <input type="text" id="plugboard-6" name="plugboard-6" minLength="2" maxLength="2" size="2" disabled={disableSettings} className="plugboard-input"></input>
+                  <input type="text" id="plugboard-7" name="plugboard-7" minLength="2" maxLength="2" size="2" disabled={disableSettings} className="plugboard-input"></input>
+                  <input type="text" id="plugboard-8" name="plugboard-8" minLength="2" maxLength="2" size="2" disabled={disableSettings} className="plugboard-input"></input>
+                  <input type="text" id="plugboard-9" name="plugboard-9" minLength="2" maxLength="2" size="2" disabled={disableSettings} className="plugboard-input"></input>
+                  <input type="text" id="plugboard-10" name="plugboard-10" minLength="2" maxLength="2" size="2" disabled={disableSettings} className="plugboard-input"></input>
                 </p>
 
                 <button id="start" onClick={start} disabled={disableSettings}>5. Start!</button>
             </div>
 
             <div>
-                <p>Message</p>
-                <textarea id="input" name="input" rows="10" cols="35" disabled></textarea>
+              <p>Message</p>
+              <textarea id="input" name="input" rows="10" cols="35" disabled></textarea>
             </div>
 
             <div>
-                <p>Output</p>
-                <textarea id="output" name="output" rows="10" cols="35" disabled></textarea>
+              <p>Output</p>
+              <textarea id="output" name="output" rows="10" cols="35" disabled></textarea>
             </div>
 
             <small>Enigma model courtesy of <a href="https://sketchfab.com/3d-models/enigma-machine-1934-c8ee76c383654e3095ea4cc9e7990274">Enigma machine, 
